57eb306b2ff0951eea5e40ad1bcd1ffb5a3be5ba
jjamgn <nguyenlamthaotrang2006@gmail.com>
2025-07-26 18:30:42 +0700

    Cập nhật

--- src/HandleInput.cpp
+++ src/HandleInput.cpp
@@ -2,44 +2,36 @@
 
 #include "HandleInput.h"
 
-HandleInput::HandleInput() {
+HandleInput::HandleInput(Player* player) : m_Player(player) {
     for (int i = 0; i < sf::Keyboard::KeyCount; ++i) {
-        keyPressed[static_cast<sf::Keyboard::Key>(i)] = false; //Chua nhan ban phim
+        keyPressed[static_cast<sf::Keyboard::Key>(i)] = false;
     }
     for (int i = 0; i < sf::Mouse::ButtonCount; ++i) {
-        mouseButtonPressed[static_cast<sf::Mouse::Button>(i)] = false; //Chua nhan chuot
+        mouseButtonPressed[static_cast<sf::Mouse::Button>(i)] = false;
     }
 }
 
-void HandleInput::update() {
-    for (int i = 0; i < sf::Keyboard::KeyCount; ++i) {
+void HandleInput::update(float deltaTime, std::vector<Enemy*>& enemies) {
+    for (int i = 0; i < sf::Keyboard::KeyCount; ++i) { //Trang thai phim
         sf::Keyboard::Key key = static_cast<sf::Keyboard::Key>(i);
-        if (sf::Keyboard::isKeyPressed(key)) {
-            keyPressed[key] = true;
-        } else {
-            keyPressed[key] = false;
-        }
+        keyPressed[key] = sf::Keyboard::isKeyPressed(key);
     }
-    for (int i = 0; i < sf::Mouse::ButtonCount; ++i) {
+    for (int i = 0; i < sf::Mouse::ButtonCount; ++i) { //Trang thai chuot
         sf::Mouse::Button button = static_cast<sf::Mouse::Button>(i);
-        if (sf::Mouse::isButtonPressed(button)) {
-            mouseButtonPressed[button] = true;
-        } else {
-            mouseButtonPressed[button] = false;
-        }
+        mouseButtonPressed[button] = sf::Mouse::isButtonPressed(button);
     }
-}
-
-bool HandleInput::isKeyPressed(sf::Keyboard::Key key) const { 
-    return keyPressed.at(key);
-}
-
-bool HandleInput::isKeyDown(sf::Keyboard::Key key) const {
-    return sf::Keyboard::isKeyPressed(key);
-}
-
-bool HandleInput::isMouseButtonPressed(sf::Mouse::Button button) const {
-    return mouseButtonPressed.at(button);
+    if (keyPressed[sf::Keyboard::Key::Left]) { //Di chuyen trai
+        m_Player->move(deltaTime, -1);
+    }
+    if (keyPressed[sf::Keyboard::Key::Right]) { //Di chuyen phai
+        m_Player->move(deltaTime, 1);
+    }
+    if (mouseButtonPressed[sf::Mouse::Button::Left] || keyPressed[sf::Keyboard::Key::Space]) { //Ban thuong
+        m_Player->shoot();
+    }
+    if (mouseButtonPressed[sf::Mouse::Button::Right] || keyPressed[sf::Keyboard::Key::Enter]) { //Su dung ki nang
+        m_Player->useSkill(enemies);
+    }
 }
 
 sf::Vector2f HandleInput::getMousePosition(const sf::RenderWindow& window) const {
